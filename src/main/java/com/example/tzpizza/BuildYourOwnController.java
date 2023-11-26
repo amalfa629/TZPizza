4523d94e78435e62b4a8ecc826b8aa69fb964c59
Tyler Amalfa <114878556+amalfa629@users.noreply.github.com>
2023-11-26 18:16:49 -0500

    .

--- src/main/java/com/example/tzpizza/BuildYourOwnController.java
+++ src/main/java/com/example/tzpizza/BuildYourOwnController.java
@@ -1,11 +1,21 @@
 package com.example.tzpizza;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+import java.util.Arrays;
 
 public class BuildYourOwnController {
     private MainMenuController mainMenuController;
+    @FXML
+    protected ListView<Topping> toppings;
     public void setMainMenuController(MainMenuController mainMenuController) {
         this.mainMenuController = mainMenuController;
+    }
+    public void initialize() {
+        toppings = new ListView<Topping>(FXCollections.observableList(Arrays.asList(Topping.values())));
     }
 }
